49a06a67f767f699d4d278a7c5dc996c5a6414c6
RC-006 <123665392+RC-006@users.noreply.github.com>
2025-10-07 14:12:35 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,15 @@
+// Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("ENTER FIRST NUMBER : ");
+    scanf("%d" , &a);
+    printf("ENTER SECOND NUMBER : ");
+    scanf("%d" , &b);
+    printf("SUM: %d\n" , a+b  );
+    printf("DIFF: %d\n" , a-b  );
+    printf("PRODUCT : %d\n" , a*b );
+    printf("QUOTIENT : %d\n" , a%b  );
+    return 0;}
